087502163eea1e64384ed383329a3b6f42aa103c
Raysa Neres <raysa.santos@estudante.ifms.edu.br>
2025-09-22 10:22:16 -0400

    #Fazendo a tarefa

--- src/produtos/produto.controller.ts
+++ src/produtos/produto.controller.ts
@@ -1,5 +1,10 @@
+import { Request, Response } from "express";
+import { db } from "../database/banco-moongo";
 
 class ProdutoController {
-   
+    async listar(req:Request, res:Response) {
+        const produtos = await db.collection('estudantes').find().toArray();
+        res.status(200).json(estudantes);
+   }
 }
 export default new ProdutoController();
