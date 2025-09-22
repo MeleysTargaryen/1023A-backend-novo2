aa152fb959d7653a8488fa5738ae2e9d0b3e88b1
Raysa Neres <raysa.santos@estudante.ifms.edu.br>
2025-09-22 10:20:53 -0400

    #Tarefa criar rota para prodrutos

--- src/rotas.ts
+++ src/rotas.ts
@@ -5,5 +5,9 @@ const rotas = Router();
 
 rotas.post("/usuarios", usuarioController.adicionar);
 rotas.get("/usuarios", usuarioController.listar);
+rotas.get("/produtos", produtoController.listar);
+//Arrumar rota de produtos
+
+
 
 export default rotas;
